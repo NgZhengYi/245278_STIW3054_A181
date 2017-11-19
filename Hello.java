dfc45fa494c4a0ec5f67d07ecd378c3c81a91217
Ng Zheng Yi <33797234+zhengyi96@users.noreply.github.com>
2017-11-19 20:42:38 +0800

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,6 +2,8 @@ package hello;
 public class Hello {
 
     public static void main(String[] args) {
+        System.out.println("Name : Ng Zheng Yi");
+        System.out.println("Matric : 245278");
         System.out.println("Hello World !");
     }
     
